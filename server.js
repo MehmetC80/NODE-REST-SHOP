8bfd44755b0ca5f494bc8e6b49b43bb7d6674c50
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-23 20:42:20 +0200

    install morgan
     install morgan middlware for getting detailted requests in the console

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-
 import express from 'express';
+import morgan from 'morgan';
 
 import { productRoutes } from './api/routes/products.js';
 import { orderRoutes } from './api/routes/orders.js';
@@ -11,6 +11,9 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use(morgan('dev'));
+
+// Routes which should handle requsts
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
