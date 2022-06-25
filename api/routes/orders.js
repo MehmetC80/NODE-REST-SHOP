6ce09d218280fd15bc87062c6ee5a1db1802e1d8
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-25 15:04:04 +0200

    update get request in orders.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,17 @@ import { Order } from '../models/orders.js';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched',
-  });
+  Order.find()
+    .exec()
+    .then((docs) => {
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post('/', (req, res, next) => {
