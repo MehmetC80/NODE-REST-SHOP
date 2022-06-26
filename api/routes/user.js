52ace6e297268e45f0875ab4e6b87b935f85024e
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-26 18:02:29 +0200

    created user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,15 +1,36 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bcript from 'bcrypt';
 
 import { User } from '../models/user.js';
 
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    email: req.body.email,
-    password: req.body.password,
+  bcript.hash(req.body.email, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).json({
+        error: err,
+      });
+    } else {
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(201).json({ message: 'User created' });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        });
+    }
   });
 });
 
