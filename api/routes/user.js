11b2e90b7e634a4796d237c848fea0f7f94e9c51
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-26 18:37:07 +0200

    fix error for empty DB

--- api/routes/user.js
+++ api/routes/user.js
@@ -11,7 +11,7 @@ router.post('/signup', (req, res, next) => {
   User.find({ email: req.body.email })
     .exec()
     .then((user) => {
-      if (user) {
+      if (user.length >= 1) {
         return res.status(409).json({
           message: 'email allready exists in the DB',
         });
